2133f195278c2a3786619fc73e8ec714080cdd08
GustavoBitencourtLopes <bitencourtgutavo007@gmail.com>
2026-04-10 20:33:01 -0300

    aula03

---
+++ aula03-modulos-condicionais/aula03-modulos.py
@@ -0,0 +1,10 @@
+import math
+
+num = 4
+raiz = math.sqrt(num)
+print(F'A raiz de {num} é {raiz:.2f}')
+
+import random
+
+num_megasena = random.randint(1,60)
+print(num_megasena)
---
+++ aula03-modulos-condicionais/aula03D-match-case.py
@@ -0,0 +1,11 @@
+escolha_usuario = 1
+
+match escolha_usuario:
+    case 0:
+        print('Sair')
+
+    case 1:
+        print('Entrar no programa')
+
+    case _:
+        print('Error')
