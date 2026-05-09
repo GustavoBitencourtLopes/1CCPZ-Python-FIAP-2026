27b791578e9b159e2d6eb4f3535fdf20c95aada0
labsfiap <bitencourtgustavo007@gmail.com>
2026-05-08 21:03:26 -0300

    aula

---
+++ aula05-strings,arrays and matrizes/aula05-matrizes.py
@@ -0,0 +1,9 @@
+musicas = [
+    ["tree little birds", "bob marley"],
+    ["no more tears", "ozzy"],
+    ["hino do SP", "SP"]
+
+]
+
+for musica in musicas:
+    print(musica)
---
+++ aula05-strings,arrays and matrizes/aula05.py
@@ -0,0 +1,5 @@
+lista_nomes = ['oi', 'tu', 'ei']
+
+for i in range(len(lista_nomes)):
+    for j in range(i+1,len(lista_nomes)):
+        print(lista_nomes[i], 'e', lista_nomes[j])
